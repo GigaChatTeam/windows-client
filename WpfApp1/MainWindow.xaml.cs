987566ac3c878b2a1a30eefa88b07f6c571729d1
ZODIAC <melenteevvadim@gmail.com>
2024-02-07 00:09:07 +0300

    added GETchannel - request

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,19 +12,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Media.Animation;
+using System.Net;
+using System.IO;
 
 namespace GigaChat
 {
     public partial class MainWindow : Window
     {
         float animationHideTime = 0.3f;
-        private bool isFullScreen = false;
-        private WindowState prevState;
 
         public MainWindow()
         {
             InitializeComponent();
+            string channels = HTTPRequests.Get("http://10.242.223.170:8084/user/@me/channels?client=5&token=Et9pMkeTo9AYVCeDmzEiLmaHxS5kxtvkqQAoXiGNnfR7nzX9");
+            if(channels != "exception")
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("произошла неизвестная ошибка, попробуйте позже");
+                Application.Current.Shutdown();
+            }
         }
 
 
@@ -67,4 +76,87 @@ namespace GigaChat
             }
         }
     }
+    public static class HTTPRequests
+    {
+        public static string Get(string url)
+        {
+            string response;
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    response = client.DownloadString(url);
+                }
+
+                return response;
+            }
+            catch (WebException ex)
+            {
+                
+                MessageBox.Show(ex.Message);
+                return "exception";
+            }
+        }
+    }
+    // Класс для объекта с заголовком и текстом
+    /*public class CustomObject : Grid
+    {
+        public CustomObject(string header, string text)
+        {
+            // Создание элементов для заголовка и текста
+            var label = new Label()
+            {
+                Content = header,
+                FontSize = 18,
+                FontWeight = FontWeights.Bold,
+                Margin = new Thickness(10)
+            };
+
+            var textBox = new TextBox()
+            {
+                Text = text,
+                IsReadOnly = true,
+                Margin = new Thickness(10)
+            };
+
+            // Добавление элементов в сетку
+            Children.Add(label);
+            Children.Add(textBox);
+
+            // Установка свойств для панельки
+            Background = Brushes.LightGray;
+            Opacity = 0.5;
+
+            // Подписка на события наведения курсора
+            MouseEnter += (s, e) => { Opacity = 1; };
+            MouseLeave += (s, e) => { Opacity = 0.5; };
+        }
+    }
+
+    public class ChannelsCheck
+    {
+        private void AddObjectToLeft_Click(object sender, RoutedEventArgs e)
+        {
+            // Создание объекта
+            var customObject = new CustomObject("Заголовок", "Текст");
+
+            // Установка выравнивания для панельки
+            customObject.HorizontalAlignment = HorizontalAlignment.Left;
+
+            // Добавление объекта в StackPanel
+            stackPanel.Children.Add(customObject);
+        }
+
+        private void AddObjectToRight_Click(object sender, RoutedEventArgs e)
+        {
+            // Создание объекта
+            var customObject = new CustomObject("Заголовок", "Текст");
+
+            // Установка выравнивания для панельки
+            customObject.HorizontalAlignment = HorizontalAlignment.Right;
+
+            // Добавление объекта в StackPanel
+            stackPanel.Children.Add(customObject);
+        }
+    }*/
 }
